c04db440fe422b041412fbc74730b2af6fae433a
Mark D Rushton <rushtonmd@gmail.com>
2014-07-03 14:51:28 -0700

    New site design

--- public/javascripts/indexscripts.js
+++ public/javascripts/indexscripts.js
@@ -1,78 +1,105 @@
 (function(mdr_cms, $, undefined) {
-	$(document).ready(function() {
-		$(".alert.alert-success.form-alert").hide();
-		$(".alert.alert-danger.form-alert").hide();
+    var queries = document.
+    querySelectorAll('.mediaquerydependent'),
+        all = queries.length,
+        cur = null,
+        attr = null;
+    return 0;
+    while (all--) {
+        cur = queries[all];
+        if (cur.dataset.media &&
+            window.matchMedia(cur.dataset.media).matches) {
+            //console.log(cur.dataset);
+            for (attr in cur.dataset) {
+                if (attr !== 'media') {
+                    cur.setAttribute(attr, cur.dataset[attr]);
+                }
+            }
+        }
+    }
+}(window.mdr_cms = window.mdr_cms || {}, jQuery));
 
-		$("#bio-link").click(function() {
-			var contentOffset = jQuery("#bio-content").offset();
-			jQuery("html,body").animate({
-				scrollTop: contentOffset.top - 50
-			}, 1000);
-			return false;
-		});
+(function(mdr_cms, $, undefined) {
 
-		$("#contact-link").click(function() {
-			var contentOffset = jQuery("#contact-content").offset();
-			jQuery("html,body").animate({
-				scrollTop: contentOffset.top
-			}, 1000);
-			return false;
-		});
+    var wow;
 
-		$("#follow-link").click(function() {
-			var contentOffset = jQuery("#follow-content").offset();
-			jQuery("html,body").animate({
-				scrollTop: contentOffset.top
-			}, 1000);
-			return false;
-		});
+    $(document).ready(function() {
 
-		$(window).resize();
-	});
-	$(window).resize(function() {
-		$("#maincontent").css("padding-top", $(".bigbg").outerHeight() + 10);
+        $(".alert.alert-success.form-alert").hide();
+        $(".alert.alert-danger.form-alert").hide();
 
-		$("#play-bio").css("height", $("#work-bio").outerHeight());
-	});
+        if ($(window).width() > 767) {
 
-	$(window).scroll(function() {
-		if ($(document).scrollTop() > 10) {
-			$(".conditional-top-nav").fadeIn(700);
-		} else {
-			$(".conditional-top-nav").fadeOut(700);
-		} // jQuery("#bottomnavigation").html( jQuery("div.bigbg").outerHeight() - jQuery(document).scrollTop());
+            $('#maincontent').fullpage({
+                afterRender: function(anchorLink, index) {
+                    wow = new WOW().init();
+                    $('.initially-hidden').show();
+                },
+                afterLoad: function(anchorLink, index) {
+                    console.log("TEST2");
+                    wow = new WOW().init();
+                }
+            });
+        } else {
+            $('.initially-hidden').show();
+        };
 
-	});
-	$('#mdr-contact-form').submit(function(event) {
+        $(window).resize();
+    });
 
-		// cancels the form submission
-		event.preventDefault();
-		$('#contact-submit-button').attr("disabled", "disabled");
+    $(window).resize(function() {
 
-		// do whatever you want here
-		$.post("contact-form", $("#mdr-contact-form").serialize())
-			.done(function(data) {
-				if (data.success) {
-					$(".alert.alert-success.form-alert").fadeIn('slow', function() {
-						$(this).delay(4000).fadeOut('slow');
-					})
-				} else {
-					$(".alert.alert-danger.form-alert").html(data.message);
-					$(".alert.alert-danger.form-alert").fadeIn('slow', function() {
-						$(this).delay(4000).fadeOut('slow');
-					});
-				}
-			})
-			.fail(function(data) {
-				$(".alert.alert-danger.form-alert").html("Oh noes! There was an error sending your message :(");
-				if (data.success) $(".alert.alert-danger.form-alert").fadeIn('slow', function() {
-					$(this).delay(4000).fadeOut('slow');
-				});
-			})
-			.always(function(data) {
-				$('#contact-submit-button').removeAttr("disabled");
-			});
+        if ($(window).width() > 767) {
+            $('.full-window-height').css('height', $(window).height());
+        } else {
+            if ($(window).width() > 600) {
+                $('.bio-content').css('height', $(window).width());
+            } else {
+                $('.bio-content').css('height', '100%');
+            }
+            $('.intro-content').css('height', $(window).height());
+
+            $('.skills-content').css('height', $(window).width());
+            $('.outro-content').css('height', $(window).height());
+
+        };
+
+        // Ensure main image is centered
+        $('.main-banner-image').css('left', $('.static-bg-image').width() / 2 - $('.main-banner-image').width() / 2);
 
 
-	});
+    });
+
+    $('#mdr-contact-form').submit(function(event) {
+
+        // cancels the form submission
+        event.preventDefault();
+        $('#contact-submit-button').attr("disabled", "disabled");
+
+        // do whatever you want here
+        $.post("contact-form", $("#mdr-contact-form").serialize())
+            .done(function(data) {
+                if (data.success) {
+                    $(".alert.alert-success.form-alert").fadeIn('slow', function() {
+                        $(this).delay(4000).fadeOut('slow');
+                    })
+                } else {
+                    $(".alert.alert-danger.form-alert").html(data.message);
+                    $(".alert.alert-danger.form-alert").fadeIn('slow', function() {
+                        $(this).delay(4000).fadeOut('slow');
+                    });
+                }
+            })
+            .fail(function(data) {
+                $(".alert.alert-danger.form-alert").html("Oh noes! There was an error sending your message :(");
+                if (data.success) $(".alert.alert-danger.form-alert").fadeIn('slow', function() {
+                    $(this).delay(4000).fadeOut('slow');
+                });
+            })
+            .always(function(data) {
+                $('#contact-submit-button').removeAttr("disabled");
+            });
+
+
+    });
 }(window.mdr_cms = window.mdr_cms || {}, jQuery));
